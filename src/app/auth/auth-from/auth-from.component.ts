241e6fddafedab594b8f491ae716bbf8e907742a
RLonskiy <RLonskiy@luxoft.com>
2018-11-26 15:58:22 +0600

    auth  form validation messages

--- src/app/auth/auth-from/auth-from.component.ts
+++ src/app/auth/auth-from/auth-from.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Credentials } from 'src/app/core/models/credentials.model';
 import { BaseFromComponent } from 'src/app/shared';
+import { Observable } from 'rxjs';
+import { map, tap, mapTo } from 'rxjs/operators';
 
 export interface AuthPayload {
   credentials: Credentials;
@@ -17,21 +19,25 @@ export class AuthFromComponent extends BaseFromComponent implements OnInit {
   @Input('authType') authType: string;
   @Output() submitForm = new EventEmitter<AuthPayload>();
 
+  $passValue: Observable<string>;
+
   constructor(
     private fb: FormBuilder,
   ) {
     super();
-   }
+  }
 
   ngOnInit() {
     this.form = this.fb.group({
-      'email': ['', Validators.required],
-      'password': ['', Validators.required]
+      'email': ['', [Validators.required, Validators.email]],
+      'password': ['', [Validators.required, Validators.minLength(6)]]
     });
 
     if (this.authType === 'register') {
       this.form.addControl('username', new FormControl('', Validators.required));
     }
+
+    this.$passValue = this.passwordControl.valueChanges;
   }
 
   submit() {
@@ -45,5 +51,41 @@ export class AuthFromComponent extends BaseFromComponent implements OnInit {
     } else {
       this.validateAllFormFields(this.form);
     }
+  }
+
+  get emailControl() {
+    return this.form.get('email');
+  }
+
+  get passwordControl() {
+    return this.form.get('password');
+  }
+
+  get usernameControl() {
+    return this.form.get('username');
+  }
+
+  get invalidEmail() {
+    return this.emailControl.hasError('email') && !this.emailControl.hasError('required');
+  }
+
+  get requiredEmail() {
+    return this.emailControl.hasError('required');
+  }
+
+  get requiredUsername() {
+    return this.usernameControl.hasError('required');
+  }
+
+  get requiredPassword() {
+    return this.passwordControl.hasError('required');
+  }
+
+  get minLengthPassword() {
+    return this.passwordControl.hasError('minlength');
+  }
+
+  get minLengthPasswordCharsLength() {
+    return this.minLengthPassword && this.passwordControl.errors.minlength.requiredLength;
   }
 }
