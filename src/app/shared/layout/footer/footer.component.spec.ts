e4e80c97e93503bcde487f97f775ce987846c5ed
RLonskiy <RLonskiy@luxoft.com>
2018-12-11 12:52:21 +0600

    footer component tests

--- src/app/shared/layout/footer/footer.component.spec.ts
+++ src/app/shared/layout/footer/footer.component.spec.ts
@@ -1,25 +1,66 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture } from '@angular/core/testing';
+import { ConfigureFn, configureTests } from '../../../lib/testing';
+import { FooterComponent } from './footer.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MaterialModule } from '../../material/material.module';
+import { RouterLinkDirectiveStubDirective } from 'src/app/lib/testing/directive/router-link-directive-stub';
 
-// import { FooterComponent } from './footer.component';
+describe('FooterComponent', () => {
+    let component: FooterComponent;
+    let fixture: ComponentFixture<FooterComponent>;
+    let de: DebugElement;
+    let linkDes;
+    let routerLinks;
 
-// describe('FooterComponent', () => {
-//   let component: FooterComponent;
-//   let fixture: ComponentFixture<FooterComponent>;
+    beforeEach(
+        async(() => {
+            const configure: ConfigureFn = testBed => {
+                testBed.configureTestingModule({
+                    declarations: [
+                        FooterComponent,
+                        RouterLinkDirectiveStubDirective
+                    ],
+                    imports: [MaterialModule]
+                });
+            };
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ FooterComponent ]
-//     })
-//     .compileComponents();
-//   }));
+            configureTests(configure).then(testBed => {
+                fixture = testBed.createComponent(FooterComponent);
+                component = fixture.componentInstance;
+                de = fixture.debugElement;
+                fixture.detectChanges();
+                linkDes = de.queryAll(By.directive(RouterLinkDirectiveStubDirective));
+                routerLinks = linkDes.map(deel => deel.injector.get(RouterLinkDirectiveStubDirective));
+            });
+        })
+    );
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(FooterComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    it('should compile', () => {
+        expect(fixture).toMatchSnapshot();
+    });
+
+    it('can get RouterLinks from template', () => {
+        fixture.detectChanges();
+
+        expect(routerLinks.length).toBe(1, 'should have 1 routerLink');
+        expect(routerLinks[0].linkParams).toEqual('/');
+    });
+
+    it('can click Heroes link in template', () => {
+        const goToDe = linkDes[0];
+        const goToLink = routerLinks[0];
+
+        expect(goToLink.navigatedTo).toBeNull();
+
+        fixture.detectChanges();
+        goToDe.triggerEventHandler('click', null);
+
+        expect(goToLink.navigatedTo).toEqual('/');
+    });
+});
+
