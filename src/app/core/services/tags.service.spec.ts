bf0bdc85add04e143113e1fff919249492ba5c9e
RLonskiy <RLonskiy@luxoft.com>
2018-12-19 18:42:36 +0600

    tags service tests

---
+++ src/app/core/services/tags.service.spec.ts
@@ -0,0 +1,40 @@
+
+import { TestBed } from '@angular/core/testing';
+import { cold } from 'jasmine-marbles';
+import { TagsService } from './tags.service';
+import { ApiService, } from './api.service';
+
+describe('TagsService', () => {
+  let service: TagsService;
+  let apiService: ApiService;
+
+  class MockApiService {
+    get = jest.fn();
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        TagsService,
+        { provide: ApiService, useValue: MockApiService }
+      ]
+    });
+
+    service = TestBed.get(TagsService);
+    apiService = TestBed.get(ApiService);
+  });
+
+  it('should create an instance successfully', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should get all tags', () => {
+    const tags = ['one', 'two', 'three'];
+    const result = cold('-a|', { a: { tags } });
+    const expected = cold('-b|', { b: tags });
+    apiService.get = jest.fn(() => result);
+
+    expect(service.getAll()).toBeObservable(expected);
+    expect(apiService.get).toHaveBeenCalledWith(`${service.BASE_URL}`);
+  });
+});
--- src/app/core/services/tags.service.ts
+++ src/app/core/services/tags.service.ts
@@ -7,12 +7,13 @@ import { pipe } from '@angular/core/src/render3/pipe';
 
 @Injectable()
 export class TagsService {
+  BASE_URL = '/tags';
   constructor(
     private apiService: ApiService
   ) { }
 
   getAll(): Observable<string[]> {
-    return this.apiService.get('/tags')
+    return this.apiService.get(this.BASE_URL)
       .pipe(map(data => data.tags));
       // trows error for test
       // .pipe(
