009c5d131670eeaf73d585d81a27e8c5cac4a907
RLonskiy <RLonskiy@luxoft.com>
2018-12-19 19:52:53 +0600

    jwt service tests

--- src/app/core/services/jwt.service.ts
+++ src/app/core/services/jwt.service.ts
@@ -2,29 +2,31 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class JwtService {
+  TOKEN_KEY = 'conduit_jwt_token';
+  USER_KEY = 'user';
 
   getToken(): string {
-    return window.localStorage['jwtToken'];
+    return window.localStorage.getItem(this.TOKEN_KEY);
   }
 
   getUser(): string {
-    return window.localStorage['user'];
+    return window.localStorage.getItem(this.USER_KEY);
   }
 
   saveToken(token: string) {
-    window.localStorage['jwtToken'] = token;
+    window.localStorage.setItem(this.TOKEN_KEY, token);
   }
 
   saveUser(user: string) {
-    window.localStorage['user'] = user;
+    window.localStorage.setItem(this.USER_KEY, user);
   }
 
   destroyToken() {
-    window.localStorage.removeItem('jwtToken');
+    window.localStorage.removeItem(this.TOKEN_KEY);
   }
 
   destroyUser() {
-    window.localStorage.removeItem('user');
+    window.localStorage.removeItem(this.USER_KEY);
   }
 
   destroyUseData() {
