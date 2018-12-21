165bf4d197eaf1c44359021ad5ac9751b21ff4dd
RLonskiy <RLonskiy@luxoft.com>
2018-12-21 23:34:16 +0600

    layout selectors tests

---
+++ src/app/layout/layout.selectors.spec.ts
@@ -0,0 +1,28 @@
+import * as fromLayout from './layout.reducer';
+import * as fromLayoutSelectors from './layout.selectors';
+import { AppState } from '../reducers';
+
+describe('Auth selectores', () => {
+  const initialAuthState: fromLayout.LayoutState = {
+    showMainLoader: false
+  };
+
+  const layoutState: fromLayout.LayoutState = initialAuthState;
+  const appState: AppState = { layout: layoutState } as AppState;
+
+  describe('selectShowMainLoader', () => {
+    it('should return "showMainLoader" state)', () => {
+      const result = fromLayoutSelectors.selectShowMainLoader(appState);
+
+      expect(result).toBe(false);
+    });
+
+    it('should return "showMainLoader" state (projector)', () => {
+      const result = fromLayoutSelectors.selectShowMainLoader.projector(
+        layoutState
+      );
+
+      expect(result).toBe(false);
+    });
+  });
+});
