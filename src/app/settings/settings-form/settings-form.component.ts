db0e4d010d2c760cb71d4a647e5478f81dc018d5
RLonskiy <RLonskiy@luxoft.com>
2018-12-24 19:30:25 +0600

    added added basic functionality to settings form

--- src/app/settings/settings-form/settings-form.component.ts
+++ src/app/settings/settings-form/settings-form.component.ts
@@ -1,38 +1,89 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { User } from 'src/app/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { BaseFromComponent } from 'src/app/shared';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-settings-form',
   templateUrl: './settings-form.component.html'
 })
-export class SettingsFormComponent implements OnChanges {
+export class SettingsFormComponent extends BaseFromComponent implements OnInit {
   @Input('disabled') disabled: boolean;
   @Input('user') user: User;
-
   @Output() updateUser = new EventEmitter<User>();
 
-  settingsForm: FormGroup;
+  $passValue: Observable<string>;
 
   constructor(
     private fb: FormBuilder
   ) {
-    this.settingsForm = this.fb.group({
-      image: '',
-      username: ['', Validators.required],
-      bio: '',
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
+    super();
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.user && changes.user.currentValue) {
-      this.settingsForm.patchValue(changes.user.currentValue);
+  ngOnInit() {
+    this.form = this.fb.group({
+      image: '',
+      username: ['', [Validators.required, Validators.maxLength(20)]],
+      bio: '',
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+
+    this.form.patchValue(this.user);
+
+    this.$passValue = this.passwordControl.valueChanges;
+  }
+
+  submit() {
+    if (this.form.valid) {
+      this.updateUser.emit(this.form.value);
+    } else {
+      this.validateAllFormFields(this.form);
     }
   }
 
-  submitForm({ value, valid }) {
-    this.updateUser.emit(value);
+  get emailControl() {
+    return this.form.get('email');
+  }
+
+  get passwordControl() {
+    return this.form.get('password');
+  }
+
+  get usernameControl() {
+    return this.form.get('username');
+  }
+
+  get invalidEmail() {
+    return this.emailControl.hasError('email') && !this.emailControl.hasError('required');
+  }
+
+  get requiredEmail() {
+    return this.emailControl.hasError('required');
+  }
+
+  get requiredUsername() {
+    return this.usernameControl.hasError('required');
+  }
+
+  get requiredPassword() {
+    return this.passwordControl.hasError('required');
+  }
+
+  get minLengthPassword() {
+    return this.passwordControl.hasError('minlength');
+  }
+
+  get maxLengthUsername() {
+    return this.usernameControl.hasError('maxlength');
+  }
+
+  get minLengthPasswordCharsLength() {
+    return this.minLengthPassword && this.passwordControl.errors.minlength.requiredLength;
+  }
+
+  get maxLengthUsernameCharsLength() {
+    return this.maxLengthUsername && this.usernameControl.errors.maxlength.requiredLength;
   }
 }
