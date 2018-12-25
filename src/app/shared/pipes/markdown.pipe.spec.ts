fe3248571638358593b534aa9424459869db0675
RLonskiy <RLonskiy@luxoft.com>
2018-12-25 17:05:45 +0600

    markdown pipe tests

---
+++ src/app/shared/pipes/markdown.pipe.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import { MarkdownPipe } from './markdown.pipe';
+
+describe('MarkdownPipe', () => {
+  const pipe = new MarkdownPipe();
+
+  it('should tramsform markdown to dom', () => {
+    expect(pipe.transform('## header').trim()).toBe('<h2 id="header">header</h2>');
+    expect(pipe.transform('# header').trim()).toBe('<h1 id="header">header</h1>');
+    expect(pipe.transform('### header').trim()).toBe('<h3 id="header">header</h3>');
+  });
+});
