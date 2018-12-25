49fbabc442b3c6c57903661692d2bb033d2aad8b
RLonskiy <RLonskiy@luxoft.com>
2018-12-25 16:47:57 +0600

    article-full component tests

--- src/app/shared/components/article-full/article-full.component.spec.ts
+++ src/app/shared/components/article-full/article-full.component.spec.ts
@@ -1,25 +1,169 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture } from '@angular/core/testing';
+import { ConfigureFn, configureTests } from '../../../lib/testing';
+import { ArticleFullComponent } from './article-full.component';
+import { DebugElement, Component, Input, Output, EventEmitter } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MaterialModule } from '../../material/material.module';
+import { Article, Profile } from 'src/app/core';
+import { getArticle } from 'src/app/lib/testing/mock-data.helpers';
 
-// import { ArticleFullComponent } from './article-full.component';
+@Component({
+    selector: 'app-article-user',
+    template: `
+    <a id="follow" (click)="onFollowedToggle()"></a>
+    <a id="favorite" (click)="onFavoriteToggle()"></a>
+    <a id="delete" (click)="onDeleteToggle()"></a>
+    `
+})
+class TestHostUserComponent {
+    @Input('canModify') canModify: boolean;
+    @Input('article') article: Article;
+    @Input('isDeletingArticle') isDeletingArticle: boolean;
+    @Output() folowing = new EventEmitter<Profile>();
+    @Output() favorited = new EventEmitter<Article>();
+    @Output() delete = new EventEmitter<any>();
 
-// describe('ArticleFullComponent', () => {
-//   let component: ArticleFullComponent;
-//   let fixture: ComponentFixture<ArticleFullComponent>;
+    onFollowedToggle(): void {
+        this.folowing.emit(this.article.author);
+    }
+    onFavoriteToggle(): void {
+        this.favorited.emit(this.article);
+    }
+    onDeleteToggle(): void {
+        this.delete.emit();
+    }
+    constructor() { }
+}
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ArticleFullComponent ]
-//     })
-//     .compileComponents();
-//   }));
+@Component({
+    selector: 'app-article-body',
+    template: ``
+})
+class TestHostBodyComponent {
+    @Input('body') body: string;
+}
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ArticleFullComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+@Component({
+    selector: 'app-list-error',
+    template: ``
+})
+class TestHostErrorsComponent {
+    @Input('errors') errors: any;
+}
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+@Component({
+    selector: 'app-tag-list',
+    template: ``
+})
+class TestHostTagsComponent {
+    @Input('tagList') tagList: any;
+}
+
+describe('ArticleFullComponent', () => {
+    let component: ArticleFullComponent;
+    let fixture: ComponentFixture<ArticleFullComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+    const initialArticle: Article = {
+        author: {},
+        tagList: []
+    } as Article;
+
+    const article: Article = getArticle();
+
+    beforeEach(
+        async(() => {
+            const configure: ConfigureFn = testBed => {
+                testBed.configureTestingModule({
+                    declarations: [
+                        ArticleFullComponent,
+                        TestHostBodyComponent,
+                        TestHostUserComponent,
+                        TestHostErrorsComponent,
+                        TestHostTagsComponent
+                    ],
+                    imports: [MaterialModule]
+                });
+            };
+
+            configureTests(configure).then(testBed => {
+                fixture = testBed.createComponent(ArticleFullComponent);
+                component = fixture.componentInstance;
+                component.article = initialArticle;
+                de = fixture.debugElement;
+                el = de.nativeElement;
+                fixture.detectChanges();
+            });
+        })
+    );
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should has "article" @Input', () => {
+        expect(component.article).toEqual(initialArticle);
+    });
+
+    it('should has "canModify" @Input', () => {
+        expect(component.canModify).toEqual(false);
+    });
+
+    it('should has "isDeletingArticle" @Input', () => {
+        expect(component.canModify).toEqual(false);
+    });
+
+    it('should has "articleErrors" @Input', () => {
+        expect(component.articleErrors).toEqual({});
+    });
+
+    it('should show "article" @Input', () => {
+        component.article = getArticle();
+        fixture.detectChanges();
+        expect(fixture).toMatchSnapshot();
+    });
+
+    it('should raise favorited event and pass an selected article', () => {
+        let selectedArticle;
+        component.article = article;
+        component.favorited.subscribe(a => selectedArticle = a);
+
+        fixture.detectChanges();
+
+        const favoriteDe = de.query(By.css('app-article-user')).query(By.css('#favorite'));
+
+        favoriteDe.triggerEventHandler('click', null);
+        expect(selectedArticle).toEqual(article);
+    });
+
+    it('should raise folowing event and pass an selected profile', () => {
+        let selectedProfile;
+        component.article = article;
+
+        component.folowing.subscribe(a => selectedProfile = a);
+
+        fixture.detectChanges();
+
+        const followDe = de.query(By.css('app-article-user')).query(By.css('#follow'));
+
+        followDe.triggerEventHandler('click', null);
+        expect(selectedProfile).toEqual(article.author);
+    });
+
+    it('should raise delete event', () => {
+        let deletedArticle;
+        component.article = article;
+        component.canModify = true;
+
+        component.delete.subscribe(a => deletedArticle = a);
+
+        fixture.detectChanges();
+
+        const deleteDe = de.query(By.css('app-article-user')).query(By.css('#delete'));
+
+        deleteDe.triggerEventHandler('click', null);
+
+        expect(deletedArticle).toEqual(article);
+    });
+});
+
--- src/app/shared/components/article-full/article-full.component.ts
+++ src/app/shared/components/article-full/article-full.component.ts
@@ -1,18 +1,17 @@
-import { Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Article, Errors } from 'src/app/core';
 import { Profile } from 'selenium-webdriver/firefox';
 
 @Component({
   selector: 'app-article-full',
   templateUrl: './article-full.component.html',
-  styleUrls: ['./article-full.component.scss'],
-  // encapsulation: ViewEncapsulation.None
+  styleUrls: ['./article-full.component.scss']
 })
 export class ArticleFullComponent {
-  @Input('article') article: Article;
-  @Input('canModify') canModify: boolean;
-  @Input('isDeletingArticle') isDeletingArticle: boolean;
-  @Input('articleErrors') articleErrors: Errors;
+  @Input('article') article: Article = {} as Article;
+  @Input('canModify') canModify = false;
+  @Input('isDeletingArticle') isDeletingArticle = false;
+  @Input('articleErrors') articleErrors: Errors = {} as Errors;
   @Output() favorited = new EventEmitter<Article>();
   @Output() folowing = new EventEmitter<Profile>();
   @Output() delete = new EventEmitter<Article>();
