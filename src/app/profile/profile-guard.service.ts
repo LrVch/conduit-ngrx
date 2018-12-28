babe7cdaf9b30c6b44fc86e6cf7d70a6cdf6d8a3
RLonskiy <RLonskiy@luxoft.com>
2018-12-28 16:26:42 +0600

    profile guard service tests

--- src/app/profile/profile-guard.service.ts
+++ src/app/profile/profile-guard.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import {
     CanActivate,
-    ActivatedRouteSnapshot,
-    RouterStateSnapshot
+    ActivatedRouteSnapshot
 } from '@angular/router';
 import { Observable, of, throwError } from 'rxjs';
-import { Store, select } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { AppState } from '../reducers';
 import { Router } from '@angular/router';
-import { tap, take, map, filter, switchMap, catchError } from 'rxjs/operators';
-import { selectProfile } from './profile.selectors';
+import { tap, take, map, switchMap, catchError } from 'rxjs/operators';
 import { ProfilesService } from '../core';
 import { ProfileLoadSuccess } from './profile.actions';
 
 @Injectable()
 export class ProfileGuard implements CanActivate {
+    NO_USER_NAME = 'No user name';
     constructor(
         private store: Store<AppState>,
         private router: Router,
@@ -23,20 +22,19 @@ export class ProfileGuard implements CanActivate {
 
     }
     canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
+        route: ActivatedRouteSnapshot
     ): Observable<boolean> {
 
         const username = route.params['username'];
 
         return this.store.pipe(
-            select(selectProfile),
             take(1),
-            switchMap(() => username ? this.profileService.get(username) : throwError('No user name')),
+            switchMap(() => username ? this.profileService.get(username) : throwError(this.NO_USER_NAME)),
             map(profile => new ProfileLoadSuccess({ profile })),
             tap((action: ProfileLoadSuccess) => this.store.dispatch(action)),
             map(profile => !!profile),
-            catchError(() => {
+            catchError((error) => {
+                console.error(error);
                 this.router.navigate(['/']);
                 return of(false);
             })
