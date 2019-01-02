8ab85bdaca6c935945f75590e8a07e18a8651eb1
RLonskiy <RLonskiy@luxoft.com>
2019-01-02 19:33:55 +0600

    hide under runaoutside

--- src/app/shared/directives/avatar.directive.ts
+++ src/app/shared/directives/avatar.directive.ts
@@ -12,44 +12,47 @@ export class AvatarDirective implements AfterViewInit, OnDestroy {
 
   constructor(
     private el: ElementRef,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private ngZone: NgZone
   ) { }
 
   ngAfterViewInit() {
-    const src = this.el.nativeElement.getAttribute('src');
-    const img = this.el.nativeElement;
+    this.ngZone.runOutsideAngular(() => {
+      const src = this.el.nativeElement.getAttribute('src');
+      const img = this.el.nativeElement;
 
-    this.renderer.addClass(img, 'img-loading');
+      this.renderer.addClass(img, 'img-loading');
 
-    // TODO: add into ngZone
-    setTimeout(() => {
-      if (!this.isImageOk()) {
-        this.renderer.addClass(img, 'img-loaded');
+      // TODO: add into ngZone
+      setTimeout(() => {
+        if (!this.isImageOk()) {
+          this.renderer.addClass(img, 'img-loaded');
+          this.renderer.setAttribute(img, 'src', this.DEFAULT_AVATAR);
+          // console.error(`image load timeout, image src is "${src}" `);
+        }
+      }, 1500);
+
+      if (src.includes('smiley-cyrus')) {
         this.renderer.setAttribute(img, 'src', this.DEFAULT_AVATAR);
-        // console.error(`image load timeout, image src is "${src}" `);
       }
-    }, 1500);
 
-    if (src.includes('smiley-cyrus')) {
-      this.renderer.setAttribute(img, 'src', this.DEFAULT_AVATAR);
-    }
+      if (this.isImageOk()) {
+        this.renderer.addClass(img, 'img-loaded-ok');
+      }
 
-    if (this.isImageOk()) {
-      this.renderer.addClass(img, 'img-loaded-ok');
-    }
+      fromEvent(img, 'load').pipe(
+        takeUntil(this.destroyStream$)
+      ).subscribe(event => {
+        this.renderer.addClass(img, 'img-loaded');
+      });
 
-    fromEvent(img, 'load').pipe(
-      takeUntil(this.destroyStream$)
-    ).subscribe(event => {
-      this.renderer.addClass(img, 'img-loaded');
-    });
-
-    fromEvent(img, 'error').pipe(
-      takeUntil(this.destroyStream$)
-    ).subscribe(event => {
-      this.renderer.addClass(img, 'img-loaded');
-      // console.error(event);
-      this.renderer.setAttribute(img, 'src', this.DEFAULT_AVATAR);
+      fromEvent(img, 'error').pipe(
+        takeUntil(this.destroyStream$)
+      ).subscribe(event => {
+        this.renderer.addClass(img, 'img-loaded');
+        // console.error(event);
+        this.renderer.setAttribute(img, 'src', this.DEFAULT_AVATAR);
+      });
     });
 
   }
