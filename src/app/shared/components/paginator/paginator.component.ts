755eb8a20db1ec5cf5dbd3a09595fa91d0217deb
RLonskiy <RLonskiy@luxoft.com>
2019-01-03 13:15:59 +0600

    hide paginator label

--- src/app/shared/components/paginator/paginator.component.ts
+++ src/app/shared/components/paginator/paginator.component.ts
@@ -8,27 +8,27 @@ import { Subject } from 'rxjs';
   template: ''
 })
 export class PaginatorComponent extends MatPaginatorIntl implements OnDestroy {
-  unsubscribe$ = new Subject<any>();
-  itemsPerPageLabel: string;
+  // unsubscribe$ = new Subject<any>();
+  itemsPerPageLabel = '';
 
   constructor(
     private translateService: TranslateService,
   ) {
     super();
 
-    this.translateService.onLangChange.pipe(
-      map(() => this.translateService.get('conduit.itemsPerPageLabel')),
-      mergeAll(),
-      takeUntil(this.unsubscribe$)
-    ).subscribe(res => {
-      this.itemsPerPageLabel = res;
-      // console.log(this.itemsPerPageLabel);
-    });
+    // this.translateService.onLangChange.pipe(
+    //   map(() => this.translateService.get('conduit.itemsPerPageLabel')),
+    //   mergeAll(),
+    //   takeUntil(this.unsubscribe$)
+    // ).subscribe(res => {
+    //   this.itemsPerPageLabel = res;
+    //   // console.log(this.itemsPerPageLabel);
+    // });
 
   }
 
   ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
+    // this.unsubscribe$.next();
+    // this.unsubscribe$.complete();
   }
 }
