fd0fd12fadce139bf1300ebcc7f929c6453184ad
RLonskiy <RLonskiy@luxoft.com>
2019-01-03 15:54:01 +0600

    transtate paginator tooltips

--- src/app/shared/components/paginator/paginator.component.ts
+++ src/app/shared/components/paginator/paginator.component.ts
@@ -8,27 +8,40 @@ import { Subject } from 'rxjs';
   template: ''
 })
 export class PaginatorComponent extends MatPaginatorIntl implements OnDestroy {
-  // unsubscribe$ = new Subject<any>();
+  unsubscribe$ = new Subject<any>();
   itemsPerPageLabel = '';
+  lastPageLabel: string;
+  firstPageLabel: string;
+  nextPageLabel: string;
+  previousPageLabel: string;
 
   constructor(
     private translateService: TranslateService,
   ) {
     super();
 
-    // this.translateService.onLangChange.pipe(
-    //   map(() => this.translateService.get('conduit.itemsPerPageLabel')),
-    //   mergeAll(),
-    //   takeUntil(this.unsubscribe$)
-    // ).subscribe(res => {
-    //   this.itemsPerPageLabel = res;
-    //   // console.log(this.itemsPerPageLabel);
-    // });
+    const soures = [
+      'conduit.paginator.firstPageLabel',
+      'conduit.paginator.lastPageLabel',
+      'conduit.paginator.nextPageLabel',
+      'conduit.paginator.previousPageLabel',
+    ];
+
+    this.translateService.onLangChange.pipe(
+      map(() => this.translateService.get(soures)),
+      mergeAll(),
+      takeUntil(this.unsubscribe$)
+    ).subscribe(res => {
+      this.firstPageLabel = res['conduit.paginator.firstPageLabel'];
+      this.lastPageLabel = res['conduit.paginator.lastPageLabel'];
+      this.nextPageLabel = res['conduit.paginator.nextPageLabel'];
+      this.previousPageLabel = res['conduit.paginator.previousPageLabel'];
+    });
 
   }
 
   ngOnDestroy() {
-    // this.unsubscribe$.next();
-    // this.unsubscribe$.complete();
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
