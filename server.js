9e76bacdc8f3093f5024c619d9b7d129c3401651
RLonskiy <RLonskiy@luxoft.com>
2019-01-10 00:03:32 +0600

    fix: add compression

--- server.js
+++ server.js
@@ -2,14 +2,15 @@
 const express = require('express');
 const path = require('path');
 const appName = 'conduit-ngrx';
+const compression = require('compression');
 
 const app = express();
 
+app.use(compression());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + `/dist/${appName}`));
 
 app.get('/*', function (req, res) {
-
     res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`));
 });
 
