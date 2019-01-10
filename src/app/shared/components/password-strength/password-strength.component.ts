d3d30c21132042314a5d1872791ee18ebeb0f8d6
RLonskiy <RLonskiy@luxoft.com>
2019-01-10 16:35:33 +0600

    fix: change passord strenght evaluation

--- src/app/shared/components/password-strength/password-strength.component.ts
+++ src/app/shared/components/password-strength/password-strength.component.ts
@@ -1,7 +1,4 @@
 import { Component, Input } from '@angular/core';
-// import * as zxcvbn from 'zxcvbn';
-
-// const zxcvbnFn = zxcvbn || (<any>window).zxcvbn;
 
 enum strength {
   worst = 1,
@@ -22,8 +19,8 @@ export class PasswordStrengthComponent {
       this.items = [];
       return;
     }
-    // this.result = zxcvbnFn(password).score + 1;
-    // this.items = Array.from({ length: this.result });
+    this.result = this.checkPassStrength(password);
+    this.items = Array.from({ length: this.result });
   }
 
   result: number;
@@ -37,5 +34,59 @@ export class PasswordStrengthComponent {
       'good-pass': this.result === strength.good,
       'strong-pass': this.result === strength.strong
     };
+  }
+
+  evaluateIt(pass: string): number {
+    let score = 0;
+    if (!pass) {
+      return score;
+    }
+
+    // award every unique letter until 5 repetitions
+    const letters = {};
+    for (let i = 0; i < pass.length; i++) {
+      letters[pass[i]] = (letters[pass[i]] || 0) + 1;
+      score += 5.0 / letters[pass[i]];
+    }
+
+    // bonus points for mixing it up
+    const variations = {
+      digits: /\d/.test(pass),
+      lower: /[a-z]/.test(pass),
+      upper: /[A-Z]/.test(pass),
+      special: /[\~\`\_\-\=\:\;\'\"\!\^\@\#\$\&\*\(\)\%\{\}\[\]\<\>,\.\?\s]/.test(
+        pass
+      ),
+      nonWords: /\W/.test(pass)
+    };
+
+    let variationCount = 0;
+    for (const check in variations) {
+      if (variations[check]) {
+        variationCount += 1;
+      } else {
+        variationCount += 0;
+      }
+    }
+    score += (variationCount - 1) * 10;
+
+    return score;
+  }
+
+  checkPassStrength(pass: string): number {
+    const score: number = this.evaluateIt(pass);
+    if (score >= 80) {
+      return 5;
+    }
+    if (score >= 70) {
+      return 4;
+    }
+    if (score >= 60) {
+      return 3;
+    }
+    if (score >= 30) {
+      return 2;
+    }
+    return 1;
   }
 }
